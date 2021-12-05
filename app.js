b47b4df701abd8d01ac01506709735d388957be3
Shehab Ahmed <shehabkhaleel@gmail.com>
2021-12-05 08:16:26 +0200

    delete fb auth

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ const session = require("express-session");
 var flash = require('connect-flash');
 const passport = require("passport")
 const passportLocalMongoose = require("passport-local-mongoose");
+
 // const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const FacebookStrategy = require('passport-facebook').Strategy;
+// const FacebookStrategy = require('passport-facebook').Strategy;
+
 const findOrCreate = require('mongoose-findorcreate')
 
 const app = express();
@@ -74,17 +76,19 @@ passport.deserializeUser(function(id, done) {
 //========================================================//
 
 //===============facebook authentication strategy===========//
-passport.use(new FacebookStrategy({
-    clientID: process.env.FACEBOOK_ID,
-    clientSecret: process.env.FACEBOOK_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/secrets"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
-));
+
+// passport.use(new FacebookStrategy({
+//     clientID: process.env.FACEBOOK_ID,
+//     clientSecret: process.env.FACEBOOK_SECRET,
+//     callbackURL: "http://localhost:3000/auth/facebook/secrets"
+//   },
+//   function(accessToken, refreshToken, profile, cb) {
+//     User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+//       return cb(err, user);
+//     });
+//   }
+// ));
+
 //==========================================================//
 
 
@@ -153,15 +157,16 @@ app.route("/register")
 //     });
     
 
-app.get('/auth/facebook',
-    passport.authenticate('facebook')
-    );
+// app.get('/auth/facebook',
+//     passport.authenticate('facebook')
+//     );
   
-app.get('/auth/facebook/secrets',
-    passport.authenticate('facebook', { failureRedirect: '/login' }),
-    function(req, res) {
-      res.redirect('/secrets');
-    });    
+// app.get('/auth/facebook/secrets',
+//     passport.authenticate('facebook', { failureRedirect: '/login' }),
+//     function(req, res) {
+//       res.redirect('/secrets');
+//     });    
+
 
 app.route("/submit")
    .get((req,res) => {
